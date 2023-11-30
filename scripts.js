7147c989b302e5c831b6e820f42de6599c3af6da
muriloxz77 <murilocarneiro63@gmail.com>
2023-11-30 14:27:59 -0300

    up

--- scripts.js
+++ scripts.js
@@ -1,34 +1,20 @@
-let chave = "sua_chave_da_API_aqui";
+let chave = "cebcd482eda57fa9a6714c1c2ba91885";
 
-function colocarNaTela(dados) {
+function colocarNaTela(dados){
   console.log(dados);
-  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.nome;
+  document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
   document.querySelector(".temp").innerHTML = Math.floor(dados.main.temp) + "°C";
   document.querySelector(".descricao").innerHTML = dados.weather[0].description;
   document.querySelector(".icone").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
-  document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
 }
 
-async function buscarCidade(cidade) {
-  let resultado = await fetch("http://localhost:3000/buscarCidade", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ cidade: cidade }),
-  });
-  if (!resultado.ok) {
-    return;
-  }
-  resultado = await resultado.json();
-  if (resultado) {
-    colocarNaTela(resultado);
-  } else {
-    console.error("Erro ao buscar a cidade:", resultado.error);
-  }
+async function buscarCidade(cidade){
+  let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=" + chave + "&lang=pt_br" + "&units=metric" )
+    .then(resposta => resposta.json());
+  colocarNaTela(dados);
 }
 
-function cliqueiNoBotao() {
+function cliqueiNoBotao(){
   let cidade = document.querySelector(".input-cidade").value;
   buscarCidade(cidade);
-}
+}''
